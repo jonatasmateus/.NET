64208687ca94e0c5aeb28b6aa35bcb8cbd8cb20f
Jonatas <jonatasmateuscontato@gmail.com>
2024-03-05 19:54:43 -0300

    Project Exercicio1 created

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade =  30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("\nProdutos:");
+            Console.WriteLine($"{produto1}, cujo preço é $ {preco1}"); 
+            Console.WriteLine($"{produto2}, cujo preço é $ {preco2}");
+
+            Console.WriteLine($"\nRegistro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+
+            Console.WriteLine($"\nMedida com oito casas decimais: {medida:F8}");
+            Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
+            Console.WriteLine($"Separador decimal invariant culture: {medida.ToString("F3", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
